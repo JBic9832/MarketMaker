8020ccdc36982e37caef5f8213e7f227ea46d967
Joseph Bickford <josephrbickford@gmail.com>
2026-03-13 21:26:37 -0700

    Fixed bug where orders would sit with zero quantity.

--- src/ProRata.h
+++ src/ProRata.h
@@ -13,31 +13,27 @@ public:
     *  This function will modify the incoming orders quantity as a side effect
     */
     void Match(OrderBook& orderBook, Order& incomingOrder) override {
-        Side side = incomingOrder.OrderSide;
-        if (side == Side::BID) {
-           matchBid(orderBook, incomingOrder); 
-        } else {
-           matchAsk(orderBook, incomingOrder); 
-        }
+		matchOrder(orderBook, incomingOrder);
 
     }
 
     // TO debug this I would see what the outcome balance for both orders is at the end and log their ids as well. Turn off all other prints to make this easier
 private:
-    void matchBid(OrderBook& orderBook, Order& incomingOrder) {
-        auto& asksAtLevel = orderBook.AsksAtLevel(incomingOrder.Price);
+    void matchOrder(OrderBook& orderBook, Order& incomingOrder) {
+		bool isBid = incomingOrder.OrderSide == Side::BID;
+        auto& levels = isBid ? orderBook.AsksAtLevel(incomingOrder.Price) : orderBook.BidsAtLevel(incomingOrder.Price);
         // Match found for price level
-        if (!asksAtLevel.empty()) {
+        if (!levels.empty()) {
             // if so prorate allocation of quantities at that level
-            const int priceLevelQuantity = orderBook.AskQuantityAtLevel(incomingOrder.Price); 
+            const int priceLevelQuantity = isBid ? orderBook.AskQuantityAtLevel(incomingOrder.Price) : orderBook.BidQuantityAtLevel(incomingOrder.Price); 
             const int initialIncomingQuantity = incomingOrder.Quantity;
 
             // For each order in the level, pro rate a percentage of the incoming order quantity
-            for (auto it = asksAtLevel.begin(); it != asksAtLevel.end();) {
+            for (auto it = levels.begin(); it != levels.end();) {
                 // Check if this is the last order
-                if (&(*it) == &asksAtLevel.back()) {
+                if (it == std::prev(levels.end())) {
                     // Remove the remaining quantity since we are at the last order
-                    if ((*it)->Quantity >= incomingOrder.Quantity) {
+                    if ((*it)->Quantity > incomingOrder.Quantity) {
                         (*it)->Quantity -= incomingOrder.Quantity;
                         incomingOrder.Quantity = 0;
                         return;
@@ -45,8 +41,7 @@ private:
                         incomingOrder.Quantity -= (*it)->Quantity;
                         (*it)->Quantity = 0;
                         // Remove the order from the book
-                        std::cout << "Should have removed order (Last order): " << (*it)->Id << std::endl;
-                        it = asksAtLevel.erase(it);
+                        it = levels.erase(it);
                         return;
                     }
                 }
@@ -59,52 +54,7 @@ private:
                 (*it)->Quantity -= share;
                 if ((*it)->Quantity <= 0) {
                     // Remove it from the book
-                    std::cout << "Should have removed order: " << (*it)->Id << std::endl;
-                    it = asksAtLevel.erase(it);
-                } else {
-                    ++it;
-                }
-                incomingOrder.Quantity -= share;
-
-            }
-        }
-    }
-
-    void matchAsk(OrderBook& orderBook, Order& incomingOrder) {
-        auto& bidsAtLevel = orderBook.BidsAtLevel(incomingOrder.Price);
-        // Match found for price level
-        if (!bidsAtLevel.empty()) {
-            // if so prorate allocation of quantities at that level
-            const int priceLevelQuantity = orderBook.BidQuantityAtLevel(incomingOrder.Price);
-            const int initialIncomingQuantity = incomingOrder.Quantity;
-
-            for (auto it = bidsAtLevel.begin(); it != bidsAtLevel.end();) {
-                if (&(*it) == &bidsAtLevel.back()) {
-                    if ((*it)->Quantity >= incomingOrder.Quantity) {
-                        (*it)->Quantity -= incomingOrder.Quantity;
-                        incomingOrder.Quantity = 0;
-                        return;
-                    } else {
-                        incomingOrder.Quantity -= (*it)->Quantity;
-                        (*it)->Quantity = 0;
-                        // Remove the order from the book
-                        std::cout << "Should have removed order (Last order): " << (*it)->Id << std::endl;
-                        it = bidsAtLevel.erase(it);
-                        return;
-                    }
-
-                }
-
-                // Find the percentage the order is granted
-                double percent = static_cast<double>((*it)->Quantity) / priceLevelQuantity;
-                int share = static_cast<int>(initialIncomingQuantity * percent);
-
-                // Take the share
-                (*it)->Quantity -= share;
-                if ((*it)->Quantity <= 0) {
-                    // Remove it from the book
-                    std::cout << "Should have removed order: " << (*it)->Id << std::endl;
-                    it = bidsAtLevel.erase(it);
+                    it = levels.erase(it);
                 } else {
                     ++it;
                 }
